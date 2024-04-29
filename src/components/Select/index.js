3e07c545a45563fe126f00d0a3572277da54146c
Zig <amalric.alex@gmail.com>
2024-04-29 16:57:39 +0200

    fixed filters

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -16,7 +16,7 @@ const Select = ({
   const [value, setValue] = useState();
   const [collapsed, setCollapsed] = useState(true);
   const changeValue = (newValue) => {
-    onChange();
+    onChange(newValue); // from onChange(), propagated newValue to eventList
     setValue(newValue);
     setCollapsed(newValue);
   };
