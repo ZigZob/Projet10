5ddf57595f008f209ae94ed0de6036e5f5db0ab8
Zig <amalric.alex@gmail.com>
2024-05-02 19:42:22 +0200

    helpers/Date test done

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,14 +1,16 @@
-/**
- * 
- */
+import { getMonth, MONTHS } from "./index";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const date = new Date("2022-01-01")
+            const result = getMonth(date);
+            expect(result).toBe("janvier" && MONTHS[1])
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const date = new Date("2022-07-08")
+            const result = getMonth(date);
+            expect(result).toBe("juillet" && MONTHS[7])
         });
     });
 })
